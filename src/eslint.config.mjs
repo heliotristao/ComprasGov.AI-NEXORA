453337903f608033c5e96e2b6557ec43ea6bed4c
heliotristao <120415397+heliotristao@users.noreply.github.com>
2025-10-24 11:25:52 +0000

    Fix ESLint config imports for Next presets

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,12 +1,16 @@
-import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
-import nextTypescript from 'eslint-config-next/typescript';
+import nextConfigs from 'eslint-config-next';
+
+const nextBaseConfig = nextConfigs.find((config) => config?.name === 'next');
+const nextTypescriptConfig = nextConfigs.find((config) => config?.name === 'next/typescript');
+const nextCoreWebVitalsConfig = nextConfigs.find((config) => config?.name === 'next/core-web-vitals');
 
 const config = [
   {
     ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-];
+  nextBaseConfig,
+  nextTypescriptConfig,
+  nextCoreWebVitalsConfig,
+].filter(Boolean);
 
 export default config;
