d059f3e56e28de18fd22512d8443d86dd68421dd
heliotristao <120415397+heliotristao@users.noreply.github.com>
2025-10-25 18:27:50 +0000

    Load local env variables when running scripts

--- scripts/run-eslint.js
+++ scripts/run-eslint.js
@@ -4,6 +4,10 @@ const { spawn } = require('node:child_process');
 const { createRequire } = require('node:module');
 const path = require('node:path');
 
+const { loadLocalEnv } = require('./load-env');
+
+loadLocalEnv();
+
 const requireFromRoot = createRequire(require.resolve('../package.json'));
 const eslintPackageJson = requireFromRoot.resolve('eslint/package.json');
 const eslintBin = path.join(path.dirname(eslintPackageJson), 'bin', 'eslint.js');
